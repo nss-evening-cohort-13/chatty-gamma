ccde13623adfd8d8b10f59520cf2c91b93884d8d
Dana Pham <danapham12@gmail.com>
2020-09-10 20:13:35 -0500

    users images are loading properly with messages

--- src/javascripts/displayMessages.js
+++ src/javascripts/displayMessages.js
@@ -5,24 +5,20 @@ const displayMessages = () => {
   const messages = data.getMessages();
   const users = userData.getUsers();
 
-  console.log(users);
-
   messages.forEach((message) => {
-    // let profilePic = '';
+    let profilePic = '';
 
-    // for (let i = 0; users.length; i + 1) {
-    //   if (users[i].name === message.user) {
-    //     profilePic = users[i].img;
-    //   }
-    // }
-    // <img src="${profilePic}"
+    for (let i = 0; i < users.length; i += 1) {
+      if (users[i].name === message.user) {
+        profilePic = users[i].img;
+      }
+    }
+
     $('#messageDisplay').append(`
     <div class="message">
       <p class="message-text">${message.text}</p>
       <div class="message-info-div">
-        <div class="user-img-container">
-
-        </div>
+        <img src=${profilePic} class="profile-pic" alt="user's profile pic">
         <p class="message-info">${message.user} ${message.time}</p>
         <img src="images/trashcan.png" class="delete-msg" alt="trashcan graphic">
       </div>
