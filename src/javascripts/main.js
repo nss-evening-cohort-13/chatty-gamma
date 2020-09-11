4fed5ab1bc03f28fc7a549470085238dcfa99174
Dana Pham <danapham12@gmail.com>
2020-09-11 11:43:53 -0500

    text size changes upon checked box

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,12 +3,14 @@ import dark from './components/darkMode';
 import userRadio from './components/userRadio';
 import displayMessages from './components/displayMessages';
 import add from './components/addMessage';
+import largeText from './components/largeText';
 
 const init = () => {
   userRadio.buildUserRadio();
   displayMessages.displayMessages();
   dark.darkMode();
   add.addMessage();
+  largeText.toggleTextSize();
 };
 
 init();
