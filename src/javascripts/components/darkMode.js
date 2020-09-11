afe189467fcb055c509dec75d3f56fd9728861d1
Chris LoJacono <clojacono92@gmail.com>
2020-09-11 09:57:07 -0500

    small changes to darkMode

--- src/javascripts/components/darkMode.js
+++ src/javascripts/components/darkMode.js
@@ -2,9 +2,17 @@ const darkMode = () => {
   $('#darkModeTarget').on('click', () => {
     if ($('#darkModeTarget').is(':checked')) {
       $('body').toggleClass('dark');
+      $('.smallCheckbox').toggleClass('dark');
+      $('nav').removeClass('navbar-light');
+      $('nav').removeClass('bg-light');
+      $('nav').toggleClass('dark');
     }
     if ($('#darkModeTarget').is(':not(:checked)')) {
       $('body').toggleClass('dark');
+      $('.smallCheckbox').toggleClass('dark');
+      $('nav').addClass('navbar-light');
+      $('nav').addClass('bg-light');
+      $('nav').toggleClass('dark');
     }
   });
 };
