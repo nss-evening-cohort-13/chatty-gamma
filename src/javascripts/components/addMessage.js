a06ba5d815717c5d6b80c17779d28fa5aa188e62
josephtmartin <goldglovejo13@gmail.com>
2020-09-10 20:22:19 -0500

    added functionality so the user can add a new message to the message board

--- src/javascripts/components/addMessage.js
+++ src/javascripts/components/addMessage.js
@@ -1,9 +1,14 @@
 import data from '../helpers/data/messageData';
 import display from './displayMessages';
 
+const emptyInput = () => {
+  $('#newMessage').val('');
+};
+
 const addMessage = () => {
   $('#newMessage').keypress((e) => {
     if (e.keyCode === 13) {
+      console.warn(e.keyCode);
       const newMessage = {
         id: data.idGenerator(),
         user: 'Ron Swanson',
@@ -11,7 +16,10 @@ const addMessage = () => {
         text: $('#newMessage').val(),
       };
       data.getMessages().push(newMessage);
-      display.displayMessages(display.getMessages());
+      console.warn(data.getMessages());
+      $('#messageDisplay').html('');
+      display.displayMessages(data.getMessages());
+      emptyInput();
     }
   });
 };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import '../styles/main.scss';
 import dark from './components/darkMode';
 import userRadio from './components/userRadio';
 import displayMessages from './components/displayMessages';
+import add from './components/addMessage';
 
 const init = () => {
   userRadio.buildUserRadio();
   displayMessages.displayMessages();
   dark.darkMode();
+  add.addMessage();
 };
 
 init();
