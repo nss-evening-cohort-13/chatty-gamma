cb08526c69baa1587e6b0fcd0b4eee7b4b95a730
josephtmartin <goldglovejo13@gmail.com>
2020-09-11 18:10:01 -0500

    have the clear button working

--- src/javascripts/components/clearMessage.js
+++ src/javascripts/components/clearMessage.js
@@ -2,10 +2,11 @@ import display from './displayMessages';
 import data from '../helpers/data/messageData';
 
 const clearMessage = () => {
-  $('#clearAllMessages').on('click', () => {
-    $('.message').remove();
-    display.displayMessages(data.getMessages());
+  $('#clearAllMessages').click(() => {
+    $('#messageDisplay').remove();
   });
+  $('#messageDisplay').html('');
+  display.displayMessages(data.getMessages());
 };
 
 export default { clearMessage };
