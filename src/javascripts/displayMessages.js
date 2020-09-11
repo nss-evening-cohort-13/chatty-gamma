6e7f1f307e0637b31840c2c6f9a9c6d9a457e49a
Dana Pham <danapham12@gmail.com>
2020-09-10 19:16:04 -0500

    added logic for profile pic

--- src/javascripts/displayMessages.js
+++ src/javascripts/displayMessages.js
@@ -1,16 +1,28 @@
 import data from './helpers/data/messageData';
+import userData from './helpers/data/userData';
 
 const displayMessages = () => {
   const messages = data.getMessages();
+  const users = userData.getUsers();
 
   messages.forEach((message) => {
+    let profilePic = '';
+
+    for (let i = 0; users.length; i + 1) {
+      if (users[i].name === message.user) {
+        profilePic = users[i].img;
+      }
+    }
+
     $('#messageDisplay').append(`
     <div class="message">
       <p class="message-text">${message.text}</p>
       <div class="message-info-div">
-        <div class="user-img-container leslie"></div>
+        <div class="user-img-container">
+          <img src="${profilePic}"
+        </div>
         <p class="message-info">${message.user} ${message.time}</p>
-        <img src="src/images/trashcan.png" class="delete-msg" alt="trashcan graphic">
+        <img src="images/trashcan.png" class="delete-msg" alt="trashcan graphic">
       </div>
     </div>`);
   });
