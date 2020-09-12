3458276f6223353e26fd2acd564508f96117764b
Dana Pham <danapham12@gmail.com>
2020-09-12 11:10:41 -0500

    saving

--- src/javascripts/components/chatbot.js
+++ src/javascripts/components/chatbot.js
@@ -1,8 +1,12 @@
 const chatbots = [
   {
     bot: 'Treat Yo Self',
-    keywords: ['treat yo self'],
-    time: ,
-    reply:
+    keywords: ['Should I buy', 'spa', "josh grobin's fish", 'celebrity fish', 'bedazzle'],
+    reply: 'Treat yo self!'
   }
-]
+];
+
+const runChatbots = () => {
+  const newMessage = $('#newMessage').val().
+  if ()
+}
