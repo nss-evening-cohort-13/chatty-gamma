31e80549de076ff3b3942385c54b1cc4b9175974
Chris LoJacono <clojacono92@gmail.com>
2020-09-12 11:06:16 -0500

    taking out unneccesary crap

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -7,8 +7,6 @@ import add from './components/addMessage';
 import largeText from './components/largeText';
 import clear from './components/clearMessage';
 
-// const xhr = $.get(`https://api.giphy.com/v1/gifs/search?api_key=${bIDzmVyydApjmjrw62c1gG8rufmN1yMA}&tag=${n.value.toLowerCase()}&limit=1`);
-
 const init = () => {
   userRadio.buildUserRadio();
   displayMessages.displayMessages();
