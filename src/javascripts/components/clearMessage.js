a0801c280cc26cde1870221f564560d920c626ca
josephtmartin <goldglovejo13@gmail.com>
2020-09-12 10:24:10 -0500

    added a clear message

--- src/javascripts/components/clearMessage.js
+++ src/javascripts/components/clearMessage.js
@@ -1,11 +1,18 @@
 import data from '../helpers/data/messageData';
 import storage from '../helpers/data/localStorage';
 
+const onClear = () => {
+  $('#messageDisplay').html(`
+    <div class="clear-message-display"><img src="images/jean-ralphio.png">The Messages Have Cleared Baby!</div>
+  `);
+};
+
 const clearMessage = () => {
   $('#clearAllMessages').on('click', () => {
     $('#messageDisplay').empty();
     data.getMessages().length = 0;
     storage.clearItems();
+    onClear();
   });
 };
 
